6ebd6fdf623288501f1c80c2a5e1db3eb0f80add
Alvaro Andrade <varito1@gmail.com>
2021-01-19 18:28:27 -0300

    Updated styles route in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { useApollo } from 'lib/apolloClient';
 
-import 'styles/globals.css';
+import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
