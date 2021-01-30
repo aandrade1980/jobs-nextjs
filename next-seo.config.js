da9538b7f0ccd3bc0252908c01546744a040a4ff
Alvaro Andrade <varito1@gmail.com>
2021-01-30 03:48:23 -0300

    Changed title to Post Jobs

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,4 +1,4 @@
-const title = 'Jobs Post – The easiest way to keep track on jobs.';
+const title = 'Post Jobs – The easiest way to keep track on jobs.';
 const description = 'Trying to learn NextJs.';
 
 const SEO = {
